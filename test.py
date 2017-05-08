35bbc1d740d11ee63d301a9d39294941785e849a
RedChueng <394124060@qq.com>
2017-05-09 07:59:07 +0800

    add two procedures in test.py

--- test.py
+++ test.py
@@ -15,3 +15,31 @@ def absolute(x):
 	return x
 
 print absolute(-5)
+
+def bigger(x, y):
+	if x < y:
+		return y
+	return x
+
+print bigger(2, 7)
+
+def is_friend(name):
+	return name[0] == 'D' or name[0] == 'N'
+
+print is_friend('Diane')
+
+print is_friend('Ned')
+
+def biggest(x, y, z):
+	if x > y:
+		if x > z:
+			return x
+		return z
+	else:
+		if y > z:
+			return y
+		return z
+
+print biggest(3, 4, 5)
+print biggest(9, 3, 4)
+print biggest(4, 6, 5)
