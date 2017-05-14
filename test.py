a24330036a64bb999edc4e3b82e6c541d3a78a18
RedChueng <394124060@qq.com>
2017-05-14 11:37:29 +0800

    add Python codes

--- test.py
+++ test.py
@@ -1,3 +1,6 @@
+# -*- coding: UTF-8 -*-
+
+
 def udacify(s):
 	return 'U' + s
 
@@ -43,3 +46,70 @@ def biggest(x, y, z):
 print biggest(3, 4, 5)
 print biggest(9, 3, 4)
 print biggest(4, 6, 5)
+
+def greatest(x, y, z):
+	if bigger(x, y) > z:
+		return bigger(x, y)
+	return z
+
+print greatest(3, 4, 5)
+print greatest(5, 2, 6)
+
+def most(x, y, z):
+	return bigger(bigger(x, y), z)
+
+print most(5, 7, 8)
+
+print max(5, 3, 2)
+
+i = 0
+while i < 10:
+	print i
+	i = i + 1
+
+print i
+
+
+def test_without_space(test):
+	test_without_space = '' #先定义一个空的字符串，也是要输出的字符串
+	while test != '': #当传入的字符串为空时，退出循环
+		if test[0] != ' ': #如果判断到字符不是空格，取出该字符存入定义好的字符串
+			test_without_space += test[0]
+		test = test[1:]
+	return test_without_space
+
+print test_without_space('how are you')
+
+
+def print_number(n):
+	i = 0
+	while i < n:
+		i += 1
+		print i
+
+print_number(3)
+
+def number(n):
+	print n
+
+x = number(5)
+
+print 'n is', x
+
+def bar(n, ch='-'):
+	return ch * n
+
+bar(3)
+
+def separator(html=False):
+	if html:
+		print "<hr />"
+	else:
+		print bar(40)
+
+def box(txt):
+	print "+ " + bar(len(txt)) + " +"
+	print "| " + txt + " |"
+	print "+ " + bar(len(txt)) + " +"
+
+box("I am Smith")
