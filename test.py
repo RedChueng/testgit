87c02056fe8273577a2e1b739783df54310ea6f3
RedChueng <394124060@qq.com>
2017-05-14 12:08:31 +0800

    修改test.py

--- test.py
+++ test.py
@@ -115,8 +115,7 @@ def box(txt):
 box("I am Smith")
 
 def weekend(day):
-	''' Define a pr
-	ocedure weekend which takes a string as its iput, and
+	''' Define a procedure weekend which takes a string as its iput, and
 	returns the boolean True if it's 'Saturday' or 'Sunday' and False otherwise '''
 	if day == 'Saturday' or day == 'Sunday':
 		return True
