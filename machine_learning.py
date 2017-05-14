43781ea965d439db8e2200e61a8319846cc1805c
RedChueng <394124060@qq.com>
2017-05-14 18:55:41 +0800

    regular learning

--- machine_learning.py
+++ machine_learning.py
@@ -188,3 +188,29 @@ def word_transformer(word):
 		return word[0]
 
 print word_transformer('name')
+
+def process_madlib(mad_lib):
+	''' Write code ofr the function process_madlib, which takes in
+	a string  "madlib" and returns the string "processed", where each
+	instance of "NOUN" is replaced with a random noun adn each instance
+	of "VERB"is replaced with a random verb. '''
+	processed = ''
+	index = 0
+	length_box = 4
+	while index < len(mad_lib):
+		frame = mad_lib[index:index + length_box]
+		to_add = word_transformer(frame)
+		processed = processed + to_add
+		if len(to_add) == 1:
+			index = index + 1
+#			print mad_lib
+		else:
+			index = index + length_box
+#			print mad_lib
+	return processed
+
+test_string_1 = "This is a good NOUN to use when you VERB your food"
+test_string_2 = "I'm going to VERB to the store and pick up a NOUN or two."
+print process_madlib(test_string_1)
+print process_madlib(test_string_2)
+
