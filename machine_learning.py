47836219bc4ce8ee41a3bda405ba325df5a987bd
RedChueng <394124060@qq.com>
2017-05-16 08:12:23 +0800

    keep learning

--- machine_learning.py
+++ machine_learning.py
@@ -372,18 +372,18 @@ def nextDay(year, month, day):
 	assume every month has 30 days. '''
 	if day < 30:
 		day += 1
-		return year,month,day
+		return year, month, day
 	else:
 		day = 1
 		if month < 12:
 			month += 1
-			return year,month,day
+			return year, month, day
 		else:
 			month = 1
 			year += 1
-			return year,month,day
+			return year, month, day
 
-print nextDay(2012,9,30)
+# print nextDay(2012,9,30)
 
 def dateIsBefore(year1,month1,day1,year2,month2,day2):
 	''' define a procedure to help deside whether the date1 is before
@@ -398,20 +398,36 @@ def dateIsBefore(year1,month1,day1,year2,month2,day2):
 				return True
 	return False
 
-print dateIsBefore(2012,10,29,2012,10,30)
+# print dateIsBefore(2012,10,29,2012,10,30)
 
 def daysBetweenDates1(year1, month1, day1, year2, month2, day2):
     """Returns the number of days between year1/month1/day1
        and year2/month2/day2. Assumes inputs are valid dates
        in Gregorian calendar, and the first date is not after
        the second."""
-        
-    # YOUR CODE HERE!
+    assert not dateIsBefore(year2, month2, day2, year1, month1, day1)
     days = 0
     while dateIsBefore(year1, month1, day1, year2, month2, day2):
-    	year1 = nextDay(year1,month1,day1)[0]
-    	month1 = nextDay(year1,month1,day1)[1]
-    	day1 = nextDay(year1,month1,day1)[2]
-        days += 1
-        
+    	year1, month1, day1 = nextDay(year1, month1, day1)
+    	days += 1
+
     return days
+
+def test():
+	test_cases = [((2012,9,30,2012,10,30),30),
+				  ((2012,1,1,2013,1,1),360),
+	              ((2012,9,1,2012,9,4),3)]
+	for (args,answer) in test_cases:
+		try:
+			result = daysBetweenDates1(*args)
+    		if result != answer:
+    			print 'Test with data:', args, 'failed'
+    		else:
+    			print 'Test case passed!'
+    	except AssertionError:
+    		if answer == 'AssertionError':
+    			print 'Nice job! Test case {0} correctly raises AssertionError\n'.format(args)
+    		else:
+    			print 'Check your work! Test case {0} should not raise AssertionError!\n'.format(args)
+
+test()
